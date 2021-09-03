042b013d51b900cfdabb6cb44e0944478395637c
Joanna Cecilia Da Silva Santos <jc_joanna@yahoo.com.br>
2021-09-02 21:55:42 -0400

    Added a sample transformation

--- HW2/transformation.js
+++ HW2/transformation.js
@@ -1,15 +1,19 @@
-function isAllElementsBelowThreshold(values, threshold){
+// Version 1: Declarative
+
+function isAllElementsBelowThresholdDecl(values, threshold){
     const isBelowThreshold = (currentValue) => currentValue < threshold;
     return values.every(isBelowThreshold);
 }
 
-// function isAllElementsBelowThreshold(values, threshold){
-//     for(let i = 0; i < values.length ; i++){
-//         if(values[i] >= threshold)
-//             return false;
-//     }
-//     return true;
-// }
+function isAllElementsBelowThresholdImp(values, threshold){
+    for(let i = 0; i < values.length ; i++){
+        if(values[i] >= threshold)
+            return false;
+    }
+    return true;
+}
 
-console.log(isAllElementsBelowThreshold([1, 30, 39, 29, 10, 13], 40));
-console.log(isAllElementsBelowThreshold([1, 30, 40, 29, 10, 13], 40));
+console.log(isAllElementsBelowThresholdDecl([1, 30, 39, 29, 10, 13], 40));
+console.log(isAllElementsBelowThresholdImp([1, 30, 39, 29, 10, 13], 40));
+console.log(isAllElementsBelowThresholdDecl([1, 30, 40, 29, 10, 13], 40));
+console.log(isAllElementsBelowThresholdImp([1, 30, 40, 29, 10, 13], 40));
