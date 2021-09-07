8007671ff8b763a4e5c851527e8d83bff5c42b7d
Joanna Cecilia Da Silva Santos <jc_joanna@yahoo.com.br>
2021-09-06 23:51:11 -0400

    Sample code for demonstrating track paths in a BFS-style graph traversal

---
+++ HW2/graphTraversal.js
@@ -0,0 +1,87 @@
+//  Pseudocode below from Wikipedia (https://en.wikipedia.org/wiki/Breadth-first_search)
+//  1  procedure BFS(G, root) is
+//  2      let Q be a queue
+//  3      label root as explored
+//  4      Q.enqueue(root)
+//  5      while Q is not empty do
+//  6          v := Q.dequeue()
+//  7          if v is the goal then
+//  8              return v
+//  9          for all edges from v to w in G.adjacentEdges(v) do
+// 10              if w is not labeled as explored then
+// 11                  label w as explored
+// 12                  Q.enqueue(w)
+
+/* 
+    keys = states; values = list of next states 
+    this is for illustration purposes, and the graph is not 100% complete 
+    (i.e., it does not have all possible states)
+*/
+
+
+function isGoal(state) {
+    return  (state[0] === 2 && state[1] === 0) ||
+            (state[1] === 2 && state[0] === 0);
+}
+/**
+ * Search WITHOUT tracking the path
+ */
+function breadthFirstSearch(graph) {
+    let rootState = [0,0]
+    let queue = [rootState]
+    let visited = new Set()
+    
+    while(queue.length > 0){
+        let currentState = queue.shift();
+        console.log(currentState);
+        if(!visited.has(currentState.toString())){
+            if(isGoal(currentState)){
+                console.log(currentState)
+                return currentState;
+            }
+            var nextStates = graph[currentState.toString()]
+            console.log(nextStates)
+
+            nextStates.forEach(x => queue.push(x));
+        }
+    }
+    return -1;
+}
+
+/**
+ * Search which tracks the path
+ */
+function breadthFirstSearch2(graph) {
+    let rootState = [0,0]
+    let queue = [ [[],rootState] ]
+    let visited = new Set()
+    
+    while(queue.length > 0){
+        let currentArray = queue.shift();
+        let currentState = currentArray[1]
+        let currentPath = currentArray[0].concat([currentState])
+
+        if(!visited.has(currentState.toString())){
+            if(isGoal(currentState)){
+                console.log(currentPath)
+                return currentState;
+            }
+            var nextStates = graph[currentState.toString()]
+            nextStates.forEach(x => queue.push([currentPath,x]));
+        }
+    }
+    return -1;
+}
+
+
+let bottlesGraph = {
+    '0,0':[[5,0],[0,3]],
+    '5,0':[[0,0],[0,3],[2,3]],
+    '0,3':[[0,0],[5,3],[3,0]],
+    '2,3':[[0,3],[2,0],[5,0]],
+    '5,3':[[0,3],[5,0]],
+    '3,0':[[0,0],[3,3]],
+    '3,3':[[3,0],[0,3],[5,1]]
+};
+
+breadthFirstSearch2(bottlesGraph);
--- HW2/transformation.js
+++ HW2/transformation.js
@@ -1,9 +1,13 @@
-function isAllElementsBelowThresholdDecl(values, threshold){
+/* EXAMPLE */
+
+// Example: Imperative
+function isAllElementsBelowThreshold(values, threshold){
     const isBelowThreshold = (currentValue) => currentValue < threshold;
     return values.every(isBelowThreshold);
 }
 
-function isAllElementsBelowThresholdImp(values, threshold){
+// Example: Converted to Declarative
+function isAllElementsBelowThreshold(values, threshold){
     for(let i = 0; i < values.length ; i++){
         if(values[i] >= threshold)
             return false;
@@ -15,3 +19,41 @@ console.log(isAllElementsBelowThresholdDecl([1, 30, 39, 29, 10, 13], 40));
 console.log(isAllElementsBelowThresholdImp([1, 30, 39, 29, 10, 13], 40));
 console.log(isAllElementsBelowThresholdDecl([1, 30, 40, 29, 10, 13], 40));
 console.log(isAllElementsBelowThresholdImp([1, 30, 40, 29, 10, 13], 40));
+
+/* FUNCTIONS TO BE CONVERTED */
+
+// Function 1
+var users = [
+    {name: "Marta", id: 1}, {name: "Josh", id: 2},
+    {name: "Achilles", id: 3}, {name: "Julius", id: 4},
+]
+function findUserById(id){
+    for(let i = 0; i < users.length; i++){
+        if(users[i].id == id)
+            return users[i].name;
+    }
+    return null;
+}
+console.log(findUserById(4));
+console.log(findUserByName(20));
+
+
+var people = [
+    {name: "Marta", height: 1.74, weight: 59}, {name: "Josh", height: 1.80, weight: 88},
+    {name: "Achilles", height: 1.68, weight: 63}, {name: "Julius", height: 1.93, weight: 97},
+];
+
+function computeBMIs(){
+    var bmiArray = [];
+    for(let i = 0; i < people.length; i++){
+        let bmi = people[i].weight/(people[i].height*people[i].height);
+        bmiArray.push(bmi);
+    }
+    return bmiArray;
+}
+
+console.log(computeBMIs());
+
+
+
+
