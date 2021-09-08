3ee8a32f64aeb00346319355268c2681ccf59131
Joanna Cecilia da Silva Santos <joanna@esc309008.dhcp.nd.edu>
2021-09-08 13:13:15 -0400

    Added promise examples

--- JavaScript/L8_Without_Promises.js
+++ JavaScript/L8_Without_Promises.js
@@ -1,26 +1,12 @@
 /**
- * A very long operation that eventually returns a random integer number from 0-10
+ * A very long operation that eventually generates a random integer number from 0-10
  */ 
 function aVeryLongOperation() {
-	let nIterations = 60;
-	for(let i = 0; i < nIterations ; i++){
-		for(let j = 0; j < nIterations ; j++){
-			for(let k = 0; k < nIterations ; k++){
-				for(let x = 0; x < nIterations ; x++){
-					for(let y = 0; y < nIterations ; y++){
-						let noop = () => {};
-					}
-				}
-			}
-		}
-	}
-	let randomInteger = Math.floor(Math.random() * 10);
-	return randomInteger; 
+	const generateRandomNum = () => {
+		let randomInt = Math.floor(Math.random() * 10);
+		console.log("Random number " + randomInt); 
+	};
+	setTimeout(generateRandomNum, 500);	
 }
 
-// Look how long this code takes
-var t0 = performance.now();
-let randomInt = aVeryLongOperation();
-console.log("Random number " + randomInt);
-var t1 = performance.now();
-console.log("Call to aVeryLongOperation() took " + (t1 - t0)/1000 + " seconds.")
+aVeryLongOperation();
