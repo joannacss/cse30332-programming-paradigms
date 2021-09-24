1e3df5cf963d1caa4d398b34c3649f7bfe00980e
Joanna Cecilia da Silva Santos <joanna@esc309008.dhcp.nd.edu>
2021-09-24 13:56:06 -0400

    Minor changes to main.js for generating HTML with a newwer version of the bootstrap library

--- IssueTrackerWebApp/main.js
+++ IssueTrackerWebApp/main.js
@@ -74,12 +74,12 @@ function fetchIssues() {
     var assignedTo = issues[i].assignedTo;
     var status = issues[i].status;
 
-    issuesList.innerHTML +=   '<div class="well">'+
+    issuesList.innerHTML +=   '<div class="container p-2 bg-light border rounded-3">'+
                               '<h6>Issue ID: ' + id + '</h6>'+
-                              '<p><span class="label label-info">' + status + '</span></p>'+
-                              '<h3>' + desc + '</h3>'+
-                              '<p><span class="glyphicon glyphicon-time"></span> ' + severity + '</p>'+
-                              '<p><span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
+                              '<p>Status: ' + status + '</p>'+
+                              '<p>Description: ' + desc + '</p>'+
+                              '<p>Severity: ' + severity + '</p>'+
+                              '<p>Assigned to: ' + assignedTo + '</p>'+
                               '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a> '+
                               '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
                               '</div>';
