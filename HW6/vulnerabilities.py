2d0d8021cfce28976637cc8f4bcdfa808e17bb75
Joanna Cecilia da Silva Santos <joanna@esc309008.dhcp.nd.edu>
2021-10-05 09:00:05 -0400

    Updated boilerplate code to make it easier to implement the solution

--- HW6/vulnerabilities.py
+++ HW6/vulnerabilities.py
@@ -14,19 +14,19 @@ def loadObjects(csv_file1, csv_file2, csv_file3):
 
 
 # Implement the queries
-def query1():
+def query1(vulnerabilities):
 	return -1
 
-def query2():
+def query2(vulnerabilities):
 	return -1
 
-def query3():
+def query3(vulnerabilities):
 	return -1
 
-def query4():
+def query4(vulnerabilities):
 	return "String"
 
-def query5():
+def query5(vulnerabilities):
 	return "String"
 
 
@@ -39,11 +39,11 @@ def main():
 	vulnerabilities = loadObjects(csv_file1, csv_file2, csv_file3)
 
 	# Print 
-	print("Query1=",query1())
-	print("Query2=",query2())
-	print("Query3=",query3())
-	print("Query4=",query4())
-	print("Query5=",query5())
+	print("Query1=",query1(vulnerabilities))
+	print("Query2=",query2(vulnerabilities))
+	print("Query3=",query3(vulnerabilities))
+	print("Query4=",query4(vulnerabilities))
+	print("Query5=",query5(vulnerabilities))
 
 
 if __name__ == '__main__':
