ce32c42bf00d52a0e6ecebcad826bbf07790eb9d
Joanna Cecilia da Silva Santos <joanna@esc309008.dhcp.nd.edu>
2021-10-13 10:09:59 -0400

    updated inline admin

--- Python/Django/mysite/polls/admin.py
+++ Python/Django/mysite/polls/admin.py
@@ -1,5 +1,23 @@
-from django.contrib import admin
+##############################################################
+### The version below is WITH inlining admin interfaces ###
+##############################################################
+# from django.contrib import admin
+# from .models import Question, Choice
 
+# class ChoiceInline(admin.TabularInline):
+#     model = Choice
+
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     model = Question
+#     inlines = [ ChoiceInline, ]
+
+# admin.site.register(Question, QuestionAdmin)
+
+##############################################################
+### The version below is WITHOUT inlining admin interfaces ###
+##############################################################
+from django.contrib import admin
 from .models import Question, Choice
 
 admin.site.register(Question)
