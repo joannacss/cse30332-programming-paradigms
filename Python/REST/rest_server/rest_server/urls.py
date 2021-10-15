df231b077f18ab328bd9f8b5648b936e4a466137
Joanna Cecilia da Silva Santos <joanna@esc309008.dhcp.nd.edu>
2021-10-15 13:40:47 -0400

    Fixed endpoint bugs

--- Python/REST/rest_server/rest_server/urls.py
+++ Python/REST/rest_server/rest_server/urls.py
@@ -1,26 +1,16 @@
-from django.urls import path, include
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
 from django.contrib import admin
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
+from django.urls import include, path
+from rest_framework import routers
+from example1 import views
 
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-# Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls), # to enable the admin interface
     path('', include(router.urls)),
-    path('api/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
