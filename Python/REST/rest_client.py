12b935131fda9eb3e6ac2db1c1389ce13718ec7d
Joanna Cecilia da Silva Santos <joanna@esc309008.dhcp.nd.edu>
2021-10-15 09:25:28 -0400

    Added REST client example

---
+++ Python/REST/rest_client.py
@@ -0,0 +1,25 @@
+import requests
+
+
+
+URL = "https://haveibeenpwned.com/api/v3/breaches?domain=linkedin.com"
+
+# parses the list of breaches
+def parse_breaches(breaches):
+	for breach in breaches:
+		print(f'Breached domain: {breach["Domain"]} on {breach["BreachDate"]}')
+		print("The following information was stolen: ")
+		for info in breach["DataClasses"]:
+			print(f"\t- {info}")
+
+def main():
+	response = requests.get(URL)
+	for attr in dir(response):
+		if not str(attr).startswith("__"): print(attr)
+
+	parse_breaches(response.json())
+	
+
+
+if __name__ == '__main__':
+	main()
