3cd73824f4c082706a5b8b770dea213fe3572c9c
Joanna Cecilia da Silva Santos <joanna@esc309008.dhcp.nd.edu>
2021-10-27 13:44:48 -0400

    Added sample code for class diagram in A13

---
+++ Ruby/Car.rb
@@ -0,0 +1,18 @@
+class Car 
+    def initialize(color, model) 
+        @color = color
+        @model = model
+    end 
+    def start() 
+        
+    end 
+    def run() 
+        
+    end 
+    def stop() 
+        
+    end 
+end 
+
+class Bus < Car
+end 
