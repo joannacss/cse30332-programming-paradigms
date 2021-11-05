e331d123025b8b2fc12eee1bbcb83fec7adc4edd
Joanna Cecilia da Silva Santos <joanna@esc309008.dhcp.nd.edu>
2021-11-05 13:39:10 -0400

    Added more examples for live DEMO

--- Java/canonicalform/Point.java
+++ Java/canonicalform/Point.java
@@ -1,6 +1,28 @@
 package canonicalform;
 public class Point {
-	private float x;
-	private float y;
+	private int x;
+	private int y;
+
+    @Override
+	public boolean equals(Object other){
+		// reflexive
+		if(other == this) 
+			return true; 
+		// non-null
+		if(other == null) 
+			return false;
+		// don't even bother! they have different types
+		if(getClass() != other.getClass()) 
+			return false;
+		Point point = (Point) other; // why do we need this type cast?
+		return x == point.x && y == point.y;
+	}
+
+	@Override
+	public int hashCode(){
+		int hash = 41 + x;
+		hash = 41 * hash + y;
+		return hash;
+	}
 
 }
