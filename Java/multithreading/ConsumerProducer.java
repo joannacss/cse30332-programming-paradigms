e49d973f2924e5e48bf530829ce877ed0a99d48f
Joanna Cecilia da Silva Santos <joanna@ESC309008.attlocal.net>
2021-11-09 23:26:39 -0500

    Code for multithreading in Java

---
+++ Java/multithreading/ConsumerProducer.java
@@ -0,0 +1,59 @@
+package multithreading;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.Random;
+class ArrayBuffer {
+    private int buffer[];
+    private int currentSize;
+
+    public ArrayBuffer() {
+        this.buffer = new int[10];
+        this.currentSize = 0; // un-needed!
+    }
+
+    public synchronized void add(int val) {
+        this.buffer[currentSize] = val;
+        this.currentSize = (currentSize + 1) % buffer.length;
+        System.out.printf("%s adding %d to position %d\n", Thread.currentThread().getName(), val, currentSize);
+    }
+
+}
+
+class NumberGeneratorTask implements Runnable {
+    private ArrayBuffer buffer; // object that is shared across threads!
+
+
+    public NumberGeneratorTask(ArrayBuffer buffer) {
+        this.buffer = buffer;
+    }
+
+    public void run() {
+        try { // put thread to sleep for sleepTime amount of time
+            int sleepTime = new Random().nextInt(5000);
+            System.out.printf("%s going to sleep for %d milliseconds.\n", Thread.currentThread().getName(), sleepTime);
+            Thread.sleep(sleepTime); // put thread to sleep
+        } catch (InterruptedException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
+
+public class ConsumerProducer {
+    private static ArrayBuffer sharedBuffer;
+
+    public static void main(String[] args) {
+        // construct the shared object
+        ArrayBuffer sharedBuffer = new ArrayBuffer();
+
+        // create two tasks to write to the shared SimpleArray
+        NumberGeneratorTask t1 = new NumberGeneratorTask(sharedBuffer);
+        NumberGeneratorTask t2 = new NumberGeneratorTask(sharedBuffer);
+
+        // execute the tasks with an ExecutorService
+        ExecutorService executor = Executors.newCachedThreadPool();
+        executor.execute(t1);
+        executor.execute(t2);
+        executor.shutdown();
+    }
+}
---
+++ Java/multithreading/PrintTask.java
@@ -0,0 +1,55 @@
+
+package multithreading;
+
+import java.util.Random;
+
+/**
+ * Example reused and modified from Java Deitel & Deitel book (9th edition).
+ * Fig. 26.3: PrintTask.java
+ * PrintTask class sleeps for a random time from 0 to 5 seconds
+ */
+public class PrintTask implements Runnable {
+    private final int sleepTime; // random sleep time for thread
+    private final String taskName; // name of task
+    private final static Random generator = new Random();
+
+    // constructor
+    public PrintTask(String name) {
+        taskName = name; // set task name
+        // pick random sleep time between 0 and 5 seconds
+        sleepTime = generator.nextInt(5000); // milliseconds
+    } // end PrintTask constructor
+
+    // method run contains the code that a thread will execute
+    public void run() {
+        try { // put thread to sleep for sleepTime amount of time
+            System.out.printf("%s going to sleep for %d milliseconds.\n",
+                    taskName, sleepTime);
+            Thread.sleep(sleepTime); // put thread to sleep
+        } catch (InterruptedException exception) {
+            System.out.printf("%s %s\n", taskName,
+                    "terminated prematurely due to interruption");
+        }
+
+        // print task name
+        System.out.printf("%s done sleeping\n", taskName);
+    } // end method run
+} // end class PrintTask
+
+class MainPrintTask {
+    public static void main(String[] args) {
+        try {
+            Thread t1 = new Thread(new PrintTask("task1"));
+            Thread t2 = new Thread(new PrintTask("task2"));
+            System.out.println("Starting the two threads");
+            t1.start();
+            t2.start();
+            System.out.println("This statement executes right away");
+            t1.join();
+            t2.join(); // waits for the threads to die.
+            System.out.println("This statement only executes after all threads finish executing");
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
