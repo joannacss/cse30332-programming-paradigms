2006c8d359a91601523e6ce6b2881b6642c2d832
Joanna Cecilia da Silva Santos <joanna@esc309008.dhcp.nd.edu>
2021-11-10 13:39:39 -0500

    Code for multithreading in Java

---
+++ Java/multithreading/SynchronizedDemo.java
@@ -0,0 +1,28 @@
+package multithreading;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.Random;
+
+
+class ArrayBuffer {
+
+
+}
+
+class NumberGeneratorTask implements Runnable {
+
+}
+
+public class MainSynchronizedDemo {
+    public static void main(String[] args) {
+        // construct the shared object
+
+
+        // create two tasks to write to the shared SimpleArray
+        
+
+        // execute the tasks with an ExecutorService
+
+    }
+}
