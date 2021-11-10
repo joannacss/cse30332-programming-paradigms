6a0caa5cf4208dc402cc74d57296cb072f64a674
joannacss <>
2021-11-10 01:48:55 +0000

    Added code for EqualsDemo

--- Java/canonicalform/EqualsDemo.java
+++ Java/canonicalform/EqualsDemo.java
@@ -13,11 +13,25 @@ class Point {
 	// Non-null: An object can never be equal to null; i.e., a.equals(null) is always false
     @Override
 	public boolean equals(Object other){
-		return false;
+		if(other == this) return true;
+		if(other == null) return false;
+		if(getClass() != other.getClass()) return false;
+		Point p = (Point) other;
+		return x == p.x && y == p.y;
 	}
 
-
-
+	@Override
+	public int hashCode(){
+		int var_code_x = x;
+		int var_code_y = y;
+		//  hash = <prime number> * hash + var_code;
+		int hash = 41 * var_code_x + var_code_y;
+		return hash;
+	}
+	// @Override
+	// public String toString(){
+	// 	return "[" + x + ", " + y + "]";
+	// }
 }
 public class EqualsDemo{
 
@@ -29,5 +43,10 @@ public class EqualsDemo{
 
 		System.out.println("p1 == p2 ? " + (p1 == p2));
 		System.out.println("p1.equals(p2) ? " + p1.equals(p2));
+		System.out.println("p2.equals(p3) ? " + p2.equals(p3));
+
+		System.out.println(p1.hashCode());
+
+		System.out.print(p1);
 	}
 }
