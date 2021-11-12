53c8235372f6a5677ae0575d9f3edf317f182166
joannacss <>
2021-11-12 15:27:23 +0000

    fixed array buffer example

--- Java/multithreading/SynchronizedExample.java
+++ Java/multithreading/SynchronizedExample.java
@@ -22,24 +22,33 @@ class ArrayBuffer {
 
 class NumberGeneratorTask implements Runnable {
     private ArrayBuffer buffer; // object that is shared across threads!
-
-    public NumberGeneratorTask(ArrayBuffer buffer) {
+	private static Random generator = new Random();
+    // constructor receives a reference to the buffer
+	public NumberGeneratorTask(ArrayBuffer buffer) {
         this.buffer = buffer;
     }
 
     public void run() {
         try {
-            // put thread to sleep for 0-5 seconds
-            int sleepTime = new Random().nextInt(5000);
-            System.out.printf("%s going to sleep for %d milliseconds.\n", Thread.currentThread().getName(), sleepTime);
-            Thread.sleep(sleepTime); // put thread to sleep
+            // random value from 1 - 100
+			int randomValue = generator.nextInt(100) + 1;
+            
+			// add to the buffer
+			this.buffer.add(randomValue);
+
+			// put thread to sleep for 0-5 seconds
+            int sleepTime = generator.nextInt(5000);
+            System.out.printf("%s will sleep for %d milliseconds\n", Thread.currentThread().getName(), sleepTime);
+
+			// put thread to sleep
+			Thread.sleep(sleepTime); 
         } catch (InterruptedException ex) {
             System.out.println(ex.getMessage());
         }
     }
 }
 
-public class MainSynchronizedExample {
+class MainSynchronizedExample {
     private static ArrayBuffer sharedBuffer;
 
     public static void main(String[] args) {
